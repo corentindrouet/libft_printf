5ddd31662877f6ad5d4a46b53fd776008e506973
corentindrouet <corentin.drouet@hotmail.fr>
2017-04-10 14:47:40 +0200

    libft_printf

---
+++ ft_memalloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdrouet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/25 10:51:52 by cdrouet           #+#    #+#             */
+/*   Updated: 2015/12/02 14:57:32 by cdrouet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char*)malloc(size);
+	if (!ptr)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+		ptr[i] = 0;
+	return ((void*)ptr);
+}
